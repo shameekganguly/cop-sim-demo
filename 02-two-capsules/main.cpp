884b40bda34eadd14c4e06c13600127840377979
Shameek Ganguly <shameekg@stanford.edu>
2020-06-19 03:29:06 -0700

    Added cop sim setup to 02-two-capsules

--- 02-two-capsules/main.cpp
+++ 02-two-capsules/main.cpp
@@ -28,19 +28,22 @@ using namespace Eigen;
 #include "force_sensor/ForceSensorSim.h"
 #include "force_sensor/ForceSensorDisplay.h"
 
-#include "lcp_solvers/LCPSolver.h"
-// #include "cop_simulator/COPSolver.h"
+#include "cop_simulator/COPSimulator.h"
 
 #include <GLFW/glfw3.h> //must be loaded after loading opengl/glew as part of graphicsinterface
 
 using namespace chai3d;
 
 const string world_fname = "resources/02-two-capsules/world.urdf";
-const string object1_fname = "resources/02-two-capsules/capsule_object.urdf";
+const string object_fname = "resources/02-two-capsules/capsule_object.urdf";
 const string object1_name = "Capsule1";
-const string object2_fname = "resources/02-two-capsules/capsule_object.urdf";
+const double capsule1_radius = 0.1;
+const double capsule2_radius = 0.1;
+const double capsule1_length = 0.2;
+const double capsule2_length = 0.2;
 const string object2_name = "Capsule2";
 const string object_link_name = "object";
+const string box_name = "Box";
 
 string camera_name = "camera_front";
 
@@ -60,7 +63,7 @@ bool f_pause_sim = false;
 
 // simulation loop
 bool fSimulationRunning = false;
-void simulation(Simulation::Sai2Simulation* sim, Sai2Model::Sai2Model* model);
+void simulation(Sai2COPSim::COPSimulator* sim);
 
 // initialize window manager
 GLFWwindow* glfwInitialize();
@@ -72,13 +75,170 @@ void glfwError(int error, const char* description);
 void keySelect(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main(int argc, char** argv) {
-	
+	const double restitution = 0.1;
+    const double friction = 0.05;
+
+    // load sai2 simulation world
+    // TODO: this is currently needed in order to load the base transform for each object
+    // as well as the transform the base plane and the gravity vector
+    // TODO: write a parser for world URDF to the cop sim world
+    auto sai2_sim = new Simulation::Sai2Simulation(world_fname, false);
+
+    Affine3d capsule1_in_world;
+    capsule1_in_world.translation() = sai2_sim->_world->getBaseNode(object1_name)->getLocalPos().eigen();
+    capsule1_in_world.linear() = sai2_sim->_world->getBaseNode(object1_name)->getLocalRot().eigen();
+    // cout << capsule1_in_world.translation().transpose() << endl;
+
+    Affine3d capsule2_in_world;
+    capsule2_in_world.translation() = sai2_sim->_world->getBaseNode(object2_name)->getLocalPos().eigen();
+    capsule2_in_world.linear() = sai2_sim->_world->getBaseNode(object2_name)->getLocalRot().eigen();
+    // cout << capsule2_in_world.translation().transpose() << endl;
+
+    Affine3d static_plane_in_world;
+    static_plane_in_world.translation() = sai2_sim->_world->getBaseNode(box_name)->getLocalPos().eigen();
+    static_plane_in_world.linear() = sai2_sim->_world->getBaseNode(box_name)->getLocalRot().eigen();
+    static_plane_in_world.translation() += static_plane_in_world.linear().col(2)*0.1/2.0; // z-height of box
+    // cout << static_plane_in_world.translation().transpose() << endl;
+
+
+    Vector3d grav_vector = sai2_sim->_world->getGravity().eigen();
+
+    // load graphics scene
+    auto graphics = new Sai2Graphics::Sai2Graphics(world_fname, false);
+    // set object graphics to wireframe, and show frame for last link
+    // graphics->showLinkFrame(true, object_name, object_link_name);
+    graphics->showWireMeshRender(true, object1_name, object_link_name);
+    graphics->showWireMeshRender(true, object2_name, object_link_name);
+
+    // load objects
+    auto coobject1 = new Sai2Model::Sai2Model(object_fname, false, capsule1_in_world, capsule1_in_world.linear().transpose()*grav_vector);
+    auto coobject2 = new Sai2Model::Sai2Model(object_fname, false, capsule2_in_world, capsule2_in_world.linear().transpose()*grav_vector);
+
+    // set up COP sim world
+    auto cop_sim = new Sai2COPSim::COPSimulator(friction, restitution);
+    //TODO: test with same primitive name
+    cop_sim->addCapsuleObject(object1_name, object_link_name, "capsule1", coobject1, capsule1_radius, capsule1_length);
+    cop_sim->addCapsuleObject(object2_name, object_link_name, "capsule2", coobject2, capsule2_radius, capsule2_length);
+    // add plane
+    cop_sim->addPlane(box_name, static_plane_in_world.linear().col(2), static_plane_in_world.translation());
+
+    // TODO: force sim/ display
+
+    // ---- INITIALIZATION COMPLETE. STARTING MULTITHREADED APP ----
+    // initialize GLFW window
+    GLFWwindow* window = glfwInitialize();
+
+    // set callbacks
+    glfwSetKeyCallback(window, keySelect);
+
+    // start the simulation thread first
+    fSimulationRunning = true;
+    thread sim_thread(simulation, cop_sim);
+
+    // TODO: visualize contact
+
+    // ---- GRAPHICS LOOP ON MAIN THREAD ----
+    Eigen::Vector3d camera_pos, camera_lookat, camera_vertical;
+    while (!glfwWindowShouldClose(window)) {
+        // update model for object. called from simulation thread
+        // coobject->updateModel();
+
+        // update graphics. this automatically waits for the correct amount of time
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        graphics->updateGraphics(object1_name, coobject1);
+        graphics->updateGraphics(object2_name, coobject2);
+        // TODO: update force display
+        // cop_force_display->update();
+
+        // render
+        graphics->render(camera_name, width, height);
+        glfwSwapBuffers(window);
+        glFinish();
+
+        // poll for events
+        glfwPollEvents();
+
+        // move scene camera as required
+        Eigen::Vector3d cam_up_axis;
+        cam_up_axis << 0.0, 0.0, 1.0; //TODO: there might be a better way to do this
+        graphics->getCameraPose(camera_name, camera_pos, camera_vertical, camera_lookat);
+        Eigen::Vector3d cam_roll_axis = (camera_lookat - camera_pos).cross(cam_up_axis);
+        Eigen::Vector3d cam_lookat_axis = camera_lookat - camera_pos;
+        cam_roll_axis.normalize();
+        cam_lookat_axis.normalize();
+        if (fTransXp) {
+            camera_pos = camera_pos + 0.05*cam_roll_axis;
+            camera_lookat = camera_lookat + 0.05*cam_roll_axis;
+        }
+        if (fTransXn) {
+            camera_pos = camera_pos - 0.05*cam_roll_axis;
+            camera_lookat = camera_lookat - 0.05*cam_roll_axis;
+        }
+        if (fTransYp) {
+            camera_pos = camera_pos + 0.05*cam_up_axis;
+            camera_lookat = camera_lookat + 0.05*cam_up_axis;
+        }
+        if (fTransYn) {
+            camera_pos = camera_pos - 0.05*cam_up_axis;
+            camera_lookat = camera_lookat - 0.05*cam_up_axis;
+        }
+        if (fTransZp) {
+            camera_pos = camera_pos + 0.05*cam_lookat_axis;
+            camera_lookat = camera_lookat + 0.05*cam_lookat_axis;
+        }
+        if (fTransZn) {
+            camera_pos = camera_pos - 0.05*cam_lookat_axis;
+            camera_lookat = camera_lookat - 0.05*cam_lookat_axis;
+        }
+        graphics->setCameraPose(camera_name, camera_pos, cam_up_axis, camera_lookat);
+    }
+
+    // ---- TEARDOWN MULTITHREADED APP ----
+    // stop simulation
+    fSimulationRunning = false;
+    sim_thread.join();
+
+    // destroy context
+    glfwDestroyWindow(window);
+
+    // terminate
+    glfwTerminate();
+
 	return 0;
 }
 
-void simulation(Simulation::Sai2Simulation* sim, Sai2Model::Sai2Model* model) {
+void simulation(Sai2COPSim::COPSimulator* sim) {
 	fSimulationRunning = true;
+
+    // create a timer
+    LoopTimer timer;
+    timer.initializeTimer();
+    timer.setLoopFrequency(20000); //1500Hz timer
+    double last_time = timer.elapsedTime(); //secs
+    bool fTimerDidSleep = true;
+    while (fSimulationRunning) {
+        fTimerDidSleep = timer.waitForNextLoop();
+
+        // integrate forward
+        double curr_time = timer.elapsedTime();
+        if(f_pause_sim) {
+            last_time = curr_time;
+            continue;
+        }
+        double loop_dt = curr_time - last_time;
+        sim->integrate(loop_dt);
+        // cout << (sim->_contact_map._islands[0]._contact_prim_pairs.front()).primA->_name << endl;
+        // cout << (sim->_contact_map._islands[0]._contact_prim_pairs.front()).primB->_name << endl;
+
+        // update last time
+        last_time = curr_time;
+    }
+    cout << "Simulation loop finished, average loop frequency: "
+        << timer.elapsedCycles()/timer.elapsedTime() << endl;
 }
+
+// TODO: add Sai2Simulation for comparison
 
 //------------------------------------------------------------------------------
 GLFWwindow* glfwInitialize() {
