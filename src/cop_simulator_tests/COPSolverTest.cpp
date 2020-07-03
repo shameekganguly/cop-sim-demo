1c2411567a3dd8f867d3c9b9273b40973650f965
Shameek Ganguly <shameekg@stanford.edu>
2020-07-03 13:44:52 -0700

    Updated tests for surface contacts

--- src/cop_simulator_tests/COPSolverTest.cpp
+++ src/cop_simulator_tests/COPSolverTest.cpp
@@ -333,4 +333,314 @@ void testLinePtCOP() {
 		);
 		printCOPSolution(sol);
 	}
+	{
+		cout << "--- TEST 8 - Patch ---" << endl;
+		cout << "No omegas" << endl;
+		cout << "No lin vels, rolling/impending" << endl;
+		MatrixXd A_constraint = MatrixXd::Identity(6, 6);
+		MatrixXd rhs_constraint = VectorXd::Zero(6);
+		rhs_constraint(2) = -0.1;
+		vector<Vector3d> omegaAs;
+		omegaAs.push_back(Vector3d::Zero());
+		vector<Vector3d> omegaBs;
+		omegaBs.push_back(Vector3d::Zero());
+		vector<Vector3d> linear_contact_vels;
+		linear_contact_vels.push_back(Vector3d::Zero());
+		COPSolver solver;
+		ContactPatch patch;
+		patch.max_extent = 0.2;
+		Circle* c = new Circle();
+		c->center.setZero();
+		c->radius = 0.1;
+		patch._intersection_curves.push_back(c);
+		patch._interior_point.setZero();
+
+		vector<uint> patch_indices;
+		patch_indices.push_back(0); // first line contact
+		vector<ContactType> contact_types;
+		contact_types.push_back(ContactType::SURFACE);
+		ContactCOPSolution sol = solver.solveSurfaceContact(
+			0.1,
+			A_constraint,
+			rhs_constraint,
+			patch,
+			patch_indices,
+			contact_types,
+			omegaAs,
+			omegaBs,
+			linear_contact_vels
+		);
+		printCOPSolution(sol);
+	}
+	{
+		cout << "--- TEST 9 - Patch ---" << endl;
+		cout << "Separating" << endl;
+		MatrixXd A_constraint = MatrixXd::Identity(6, 6);
+		MatrixXd rhs_constraint = VectorXd::Zero(6);
+		rhs_constraint(2) = 0.5;
+		rhs_constraint(3) = 1.0;
+		vector<Vector3d> omegaAs;
+		omegaAs.push_back(Vector3d::Zero());
+		vector<Vector3d> omegaBs;
+		omegaBs.push_back(Vector3d::Zero());
+		vector<Vector3d> linear_contact_vels;
+		linear_contact_vels.push_back(Vector3d::Zero());
+		COPSolver solver;
+		ContactPatch patch;
+		patch.max_extent = 0.2;
+		Circle* c = new Circle();
+		c->center.setZero();
+		c->radius = 0.1;
+		patch._intersection_curves.push_back(c);
+		patch._interior_point.setZero();
+
+		vector<uint> patch_indices;
+		patch_indices.push_back(0); // first line contact
+		vector<ContactType> contact_types;
+		contact_types.push_back(ContactType::SURFACE);
+		ContactCOPSolution sol = solver.solveSurfaceContact(
+			0.1,
+			A_constraint,
+			rhs_constraint,
+			patch,
+			patch_indices,
+			contact_types,
+			omegaAs,
+			omegaBs,
+			linear_contact_vels
+		);
+		printCOPSolution(sol);
+	}
+	{
+		cout << "--- TEST 10 - Patch ---" << endl;
+		cout << "Tilting X moment" << endl;
+		MatrixXd A_constraint = MatrixXd::Identity(6, 6);
+		MatrixXd rhs_constraint = VectorXd::Zero(6);
+		rhs_constraint(2) = -0.1;
+		rhs_constraint(3) = 1.0;
+		vector<Vector3d> omegaAs;
+		omegaAs.push_back(Vector3d::Zero());
+		vector<Vector3d> omegaBs;
+		omegaBs.push_back(Vector3d::Zero());
+		vector<Vector3d> linear_contact_vels;
+		linear_contact_vels.push_back(Vector3d::Zero());
+		COPSolver solver;
+		ContactPatch patch;
+		patch.max_extent = 0.2;
+		Circle* c = new Circle();
+		c->center.setZero();
+		c->radius = 0.1;
+		patch._intersection_curves.push_back(c);
+		patch._interior_point.setZero();
+
+		vector<uint> patch_indices;
+		patch_indices.push_back(0); // first line contact
+		vector<ContactType> contact_types;
+		contact_types.push_back(ContactType::SURFACE);
+		ContactCOPSolution sol = solver.solveSurfaceContact(
+			0.1,
+			A_constraint,
+			rhs_constraint,
+			patch,
+			patch_indices,
+			contact_types,
+			omegaAs,
+			omegaBs,
+			linear_contact_vels
+		);
+		printCOPSolution(sol);
+	}
+	{
+		cout << "--- TEST 11 - Patch ---" << endl;
+		cout << "Tilting Y moment" << endl;
+		MatrixXd A_constraint = MatrixXd::Identity(6, 6);
+		MatrixXd rhs_constraint = VectorXd::Zero(6);
+		rhs_constraint(2) = -0.1;
+		rhs_constraint(4) = 1.0;
+		vector<Vector3d> omegaAs;
+		omegaAs.push_back(Vector3d::Zero());
+		vector<Vector3d> omegaBs;
+		omegaBs.push_back(Vector3d::Zero());
+		vector<Vector3d> linear_contact_vels;
+		linear_contact_vels.push_back(Vector3d::Zero());
+		COPSolver solver;
+		ContactPatch patch;
+		patch.max_extent = 0.2;
+		Circle* c = new Circle();
+		c->center.setZero();
+		c->radius = 0.1;
+		patch._intersection_curves.push_back(c);
+		patch._interior_point.setZero();
+
+		vector<uint> patch_indices;
+		patch_indices.push_back(0); // first line contact
+		vector<ContactType> contact_types;
+		contact_types.push_back(ContactType::SURFACE);
+		ContactCOPSolution sol = solver.solveSurfaceContact(
+			0.1,
+			A_constraint,
+			rhs_constraint,
+			patch,
+			patch_indices,
+			contact_types,
+			omegaAs,
+			omegaBs,
+			linear_contact_vels
+		);
+		printCOPSolution(sol);
+	}
+	{
+		cout << "--- TEST 12 - Patch ---" << endl;
+		cout << "Linear slip" << endl;
+		MatrixXd A_constraint = MatrixXd::Identity(6, 6);
+		MatrixXd rhs_constraint = VectorXd::Zero(6);
+		rhs_constraint(2) = -0.1;
+		vector<Vector3d> omegaAs;
+		omegaAs.push_back(Vector3d::Zero());
+		vector<Vector3d> omegaBs;
+		omegaBs.push_back(Vector3d::Zero());
+		vector<Vector3d> linear_contact_vels;
+		linear_contact_vels.push_back(Vector3d(0.1, 0, 0));
+		COPSolver solver;
+		ContactPatch patch;
+		patch.max_extent = 0.2;
+		Circle* c = new Circle();
+		c->center.setZero();
+		c->radius = 0.1;
+		patch._intersection_curves.push_back(c);
+		patch._interior_point.setZero();
+
+		vector<uint> patch_indices;
+		patch_indices.push_back(0); // first line contact
+		vector<ContactType> contact_types;
+		contact_types.push_back(ContactType::SURFACE);
+		ContactCOPSolution sol = solver.solveSurfaceContact(
+			0.1,
+			A_constraint,
+			rhs_constraint,
+			patch,
+			patch_indices,
+			contact_types,
+			omegaAs,
+			omegaBs,
+			linear_contact_vels
+		);
+		printCOPSolution(sol);
+	}
+	{
+		cout << "--- TEST 13 - Patch ---" << endl;
+		cout << "Rotational slip" << endl;
+		MatrixXd A_constraint = MatrixXd::Identity(6, 6);
+		MatrixXd rhs_constraint = VectorXd::Zero(6);
+		rhs_constraint(2) = -0.1;
+		vector<Vector3d> omegaAs;
+		omegaAs.push_back(Vector3d::Zero());
+		vector<Vector3d> omegaBs;
+		omegaBs.push_back(Vector3d(0, 0, 0.1));
+		vector<Vector3d> linear_contact_vels;
+		linear_contact_vels.push_back(Vector3d(0.0, 0, 0));
+		COPSolver solver;
+		ContactPatch patch;
+		patch.max_extent = 0.2;
+		Circle* c = new Circle();
+		c->center.setZero();
+		c->radius = 0.1;
+		patch._intersection_curves.push_back(c);
+		patch._interior_point.setZero();
+
+		vector<uint> patch_indices;
+		patch_indices.push_back(0); // first line contact
+		vector<ContactType> contact_types;
+		contact_types.push_back(ContactType::SURFACE);
+		ContactCOPSolution sol = solver.solveSurfaceContact(
+			0.1,
+			A_constraint,
+			rhs_constraint,
+			patch,
+			patch_indices,
+			contact_types,
+			omegaAs,
+			omegaBs,
+			linear_contact_vels
+		);
+		printCOPSolution(sol);
+	}
+	{
+		cout << "--- TEST 14 - Patch ---" << endl;
+		cout << "Impending translational slip" << endl;
+		MatrixXd A_constraint = MatrixXd::Identity(6, 6);
+		MatrixXd rhs_constraint = VectorXd::Zero(6);
+		rhs_constraint(2) = -0.1;
+		rhs_constraint(1) = 0.1;
+		vector<Vector3d> omegaAs;
+		omegaAs.push_back(Vector3d::Zero());
+		vector<Vector3d> omegaBs;
+		omegaBs.push_back(Vector3d(0, 0, 0.0));
+		vector<Vector3d> linear_contact_vels;
+		linear_contact_vels.push_back(Vector3d(0.0, 0, 0));
+		COPSolver solver;
+		ContactPatch patch;
+		patch.max_extent = 0.2;
+		Circle* c = new Circle();
+		c->center.setZero();
+		c->radius = 0.1;
+		patch._intersection_curves.push_back(c);
+		patch._interior_point.setZero();
+
+		vector<uint> patch_indices;
+		patch_indices.push_back(0); // first line contact
+		vector<ContactType> contact_types;
+		contact_types.push_back(ContactType::SURFACE);
+		ContactCOPSolution sol = solver.solveSurfaceContact(
+			0.1,
+			A_constraint,
+			rhs_constraint,
+			patch,
+			patch_indices,
+			contact_types,
+			omegaAs,
+			omegaBs,
+			linear_contact_vels
+		);
+		printCOPSolution(sol);
+	}
+	{
+		cout << "--- TEST 15 - Patch ---" << endl;
+		cout << "Impending rotational slip" << endl;
+		MatrixXd A_constraint = MatrixXd::Identity(6, 6);
+		MatrixXd rhs_constraint = VectorXd::Zero(6);
+		rhs_constraint(2) = -0.1;
+		rhs_constraint(5) = 0.1;
+		vector<Vector3d> omegaAs;
+		omegaAs.push_back(Vector3d::Zero());
+		vector<Vector3d> omegaBs;
+		omegaBs.push_back(Vector3d(0, 0, 0.0));
+		vector<Vector3d> linear_contact_vels;
+		linear_contact_vels.push_back(Vector3d(0.0, 0, 0));
+		COPSolver solver;
+		ContactPatch patch;
+		patch.max_extent = 0.2;
+		Circle* c = new Circle();
+		c->center.setZero();
+		c->radius = 0.1;
+		patch._intersection_curves.push_back(c);
+		patch._interior_point.setZero();
+
+		vector<uint> patch_indices;
+		patch_indices.push_back(0); // first line contact
+		vector<ContactType> contact_types;
+		contact_types.push_back(ContactType::SURFACE);
+		ContactCOPSolution sol = solver.solveSurfaceContact(
+			0.1,
+			A_constraint,
+			rhs_constraint,
+			patch,
+			patch_indices,
+			contact_types,
+			omegaAs,
+			omegaBs,
+			linear_contact_vels
+		);
+		printCOPSolution(sol);
+	}
 }
